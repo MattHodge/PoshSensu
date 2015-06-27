1412040775917179d446238b0aa493f11be63356
unknown <matthodge@gmail.com>
2015-06-27 14:51:09 -0500

    Make logging simpler and cut down on amount of messages.

--- Functions/Start-SensuChecks.ps1
+++ Functions/Start-SensuChecks.ps1
@@ -31,7 +31,7 @@ function Start-SensuChecks
     # $Config.check_groups is ordered by max_execution_time
     ForEach ($checkgroup in $Config.check_groups)
     {   
-        Write-PSLog @loggingDefaults -Method DEBUG -Message "Verifiying Checks for $($checkgroup.group_name)"
+        Write-PSLog @loggingDefaults -Method DEBUG -Message "Verifiying Checks ::: Group Name: $($checkgroup.group_name)"
 
         # Create the framework commands for the job
         $jobCommands = @()
@@ -53,7 +53,7 @@ function Start-SensuChecks
         # Scale the ttl back by 4.5% to ensure that the checks in the background job complete in time
         $scaledTTL = $checkgroup.ttl - ($checkgroup.ttl * 0.045)
 
-        $jobCommands += 'Write-PSLog @loggingDefaults -Method DEBUG -Message "TTL for checks in this group: {0}s. Scaled TTL for this group: {1}s"' -f $checkgroup.ttl,$scaledTTL
+        $jobCommands += 'Write-PSLog @loggingDefaults -Method DEBUG -Message "TTLs ::: Check Group: {0}s Check Group Scaled: {1}s"' -f $checkgroup.ttl,$scaledTTL
                     
         # Validates each check first
         ForEach ($check in $checkgroup.checks)
@@ -61,17 +61,14 @@ function Start-SensuChecks
             $checkPath = (Join-Path -Path $Config.checks_directory -ChildPath $check.command)
             # Using this instead of Resolve-Path so any warnings can provide the full path to the expected check location
             $checkScriptPath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($checkPath)
-            Write-PSLog @loggingDefaults -Method DEBUG -Message "Looking for check at '$checkScriptPath'"
+            Write-PSLog @loggingDefaults -Method DEBUG -Message "Check Search Location ::: '$checkScriptPath'"
         
             # Check if the check actually exists
             if (Test-Path -Path $checkScriptPath)
             {
                 $jobToBeRun++
 
-                Write-PSLog @loggingDefaults -Method DEBUG -Message "Current count of jobs to be run in this check group: $jobToBeRun"
-                Write-PSLog @loggingDefaults -Method DEBUG -Message "Added Check to Job:"
-                Write-PSLog @loggingDefaults -Method DEBUG -Message "  Check Name: $($check.Name)"
-                Write-PSLog @loggingDefaults -Method DEBUG -Message "  Check Path: $($checkScriptPath)"
+                Write-PSLog @loggingDefaults -Method DEBUG -Message "Check Added ::: Number: $jobToBeRun Name: $($check.Name) Path: $($checkScriptPath)"
 
                 # Build the command to run the collect and check the output
                 # An Example:
@@ -81,12 +78,12 @@ function Start-SensuChecks
                 $jobCommands += '$returnObject.{0} = . ''{1}'' {2}' -f $check.Name,$checkScriptPath,$check.arguments # Dot sources the check .ps1 and passes arguments
                 $jobCommands += '}' # End of try block for the check
                 $jobCommands += 'catch { Write-PSLog @loggingDefaults -Method WARN -Message "$_" }' # Log errors that occur in the check
-                $jobCommands += 'Write-PSLog @loggingDefaults -Method DEBUG -Message "The check {0} took $($stopwatch.Elapsed.Milliseconds) milliseconds to execute"' -f $check.Name
+                $jobCommands += 'Write-PSLog @loggingDefaults -Method DEBUG -Message "Check Complete ::: Name: {0} Execution Time: $($stopwatch.Elapsed.Milliseconds)ms"' -f $check.Name
                 $jobCommands += '[System.GC]::Collect()'
             }
             else
             {
-                Write-PSLog @loggingDefaults -Method WARN -Message "Check Load Error: Unable to find check '$($check.Name)'. Not adding to the job"
+                Write-PSLog @loggingDefaults -Method WARN -Message "Check Not Found ::: Name: '$($check.Name)' Path: '$($checkScriptPath)'."
             }
         }
 
@@ -95,13 +92,13 @@ function Start-SensuChecks
         {          
             $jobCommands += 'Write-Output $returnObject' # 
             $jobCommands += '$stopwatch.Stop()'
-            $jobCommands += 'Write-PSLog @loggingDefaults -Method DEBUG -Message "Total time taken for all checks in the group: $($stopwatch.Elapsed.Milliseconds)ms"'
+            $jobCommands += 'Write-PSLog @loggingDefaults -Method DEBUG -Message "Check Group Complete ::: Total Execution Time: $($stopwatch.Elapsed.Milliseconds)ms"'
             $jobCommands += '$timeToSleep = {0} - $stopwatch.Elapsed.Seconds' -f $scaledTTL
-            $jobCommands += 'if ($stopwatch.Elapsed.Seconds -lt {0}) {{ Start-Sleep -Seconds $timeToSleep ; Write-PSLog @loggingDefaults -Method DEBUG -Message "Sleeping check group for $($timeToSleep)s before starting the checks again" }}' -f $scaledTTL # Wait until the TTL has been reached for the check.
-            $jobCommands += 'else { Write-Warning "Job took longer than ttl! Starting Immediately" }'
+            $jobCommands += 'if ($stopwatch.Elapsed.Seconds -lt {0}) {{ Start-Sleep -Seconds $timeToSleep ; Write-PSLog @loggingDefaults -Method DEBUG -Message "Sleeping Check Group :::  Sleep Time: $($timeToSleep)s" }}' -f $scaledTTL # Wait until the TTL has been reached for the check.
+            $jobCommands += 'else { Write-Warning "Job Took Longer Than TTL! Starting It Again Immediately" }'
             $jobCommands += '}'
             
-            Write-PSLog @loggingDefaults -Method INFO -Message "Creating background job for check group ""$($checkgroup.group_name)"""
+            Write-PSLog @loggingDefaults -Method INFO -Message "Creating Background Job ::: Check Group: $($checkgroup.group_name)"
 
             # Split the array into new lines and create a script block        
             $scriptBlock = [Scriptblock]::Create($jobCommands -join "`r`n")
@@ -110,12 +107,12 @@ function Start-SensuChecks
             $initScriptForJob = "Import-Module '$($modulePath)'"
             $initScriptForJob = [scriptblock]::Create($initScriptForJob)
 
-            # Start background job
+            # Start background job. InitializationScript loads the PoshSensu module
             $backgroundJobs += Start-BackgroundCollectionJob -Name "$($checkgroup.group_name)" -ScriptBlock $scriptBlock -InitializationScript $initScriptForJob
         }
         else
         {
-            Write-PSLog @loggingDefaults -Method WARN -Message "Check group ""$($checkgroup.group_name)"" has no valid checks. Not creating background job."
+            Write-PSLog @loggingDefaults -Method WARN -Message "Group Has No Valid Checks ::: Check Group: $($checkgroup.group_name)"
         }
     }
 
@@ -143,29 +140,28 @@ function Start-SensuChecks
                     # If there is a property on job result matching the check name
                     if (Get-Member -InputObject $jobResult -Name $check.name -MemberType Properties)
                     {
-                        Write-PSLog @loggingDefaults -Method DEBUG -Message "Check ""$($check.name)"" has a result! Merging data from the configuration file with the check result."
+                        Write-PSLog @loggingDefaults -Method DEBUG -Message "Check Result Returned. Merging Data From Config File ::: Check Name: $($check.name)"
 
                         # Merge all the data about the job and return it
                         $finalCheckResult = Merge-HashtablesAndObjects -InputObjects $jobResult.($check.name),$ChecksToValidate,$check -ExcludeProperties 'checks' | ConvertTo-Json -Compress
-                        Write-PSLog @loggingDefaults -Method DEBUG -Message "Check Result: $finalCheckResult"
+                        Write-PSLog @loggingDefaults -Method DEBUG -Message "Check Result ::: Check Name: $($check.name) Result: $finalCheckResult"
 
                         $finalCheckResult | Send-DataTCP -ComputerName $Config.sensu_socket_ip -Port $Config.sensu_socket_port
                     }
                     else
                     {
-                        Write-PSLog @loggingDefaults -Method WARN -Message "Check ""$($check.name)"" does not have a result! Verify the test script exists or it returns a result when run manually."
+                        Write-PSLog @loggingDefaults -Method WARN -Message "Check Has No Result ::: Check Name: $($check.name) Additonal Help: Verify the check by running it manually out side of PoshSensu"
                     }
                 }
             }
         }
 
         $stopwatch.Stop()
-        Write-PSLog @loggingDefaults -Method INFO -Message "Total Execution Time For $($backgroundJobs.Length) Background Job(s): $($stopwatch.Elapsed.Seconds)s"
-
+        
         # Sleep for the smallest ttl minus how long this run took
         $lowestTTL = ($Config.check_groups.ttl | Sort-Object)[0]
         $sleepTime = ($lowestTTL - $stopwatch.Elapsed.TotalSeconds)
-        Write-PSLog @loggingDefaults -Method DEBUG -Message "All processing has finished for the background jobs. Sleeping for $($sleepTime)s"
+        Write-PSLog @loggingDefaults -Method INFO -Message "All Background Jobs Complete ::: Total Background Job(s): $($backgroundJobs.Length) Total Time Taken: $($stopwatch.Elapsed.Seconds)s Sleeping For: $($sleepTime)s"
         Start-Sleep -Seconds ($lowestTTL - $stopwatch.Elapsed.TotalSeconds)
     }
     

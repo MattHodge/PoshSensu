5234963c83da970b35237204e2bc572dfe8a5381
unknown <matthodge@gmail.com>
2015-06-29 17:44:19 -0500

    adding a cpu usage check and updating the check_service check

---
+++ Checks/check_cpu_usage.ps1
@@ -0,0 +1,40 @@
+﻿Param
+(
+    # Warning Threshold 
+    [Parameter(Mandatory=$false)]
+    [int]
+    $WarningThreshold = 80,
+
+    # Error Threshold 
+    [Parameter(Mandatory=$false)]
+    [int]
+    $ErrorThreshold = 90
+)
+
+$cpu = Get-WmiObject win32_processor
+
+if ($cpu.LoadPercentage -ge $ErrorThreshold)
+{
+    $sensu_status = 2
+    $output = "CPU Load Percentage Over Error Threshold of $($ErrorThreshold)% - Currently $($cpu.LoadPercentage)%"
+}
+elseif ($cpu.LoadPercentage -gt $WarningThreshold)
+{
+    $sensu_status = 1
+    $output = "CPU Load Percentage Over Warning Threshold of $($WarningThreshold)% - Currently $($cpu.LoadPercentage)%"
+}
+else
+{
+    $sensu_status = 0
+    $output = "CPU Load Percentage OK - $($cpu.LoadPercentage)%"
+}
+
+$metricObject = @{
+    cpu_name = $cpu.Name
+    cpu_cores = $cpu.NumberOfCores
+    cpu_logicalprocessors = $cpu.NumberOfLogicalProcessors
+    output = $output
+    status = $sensu_status
+}
+
+return $metricObject
--- Checks/check_service.ps1
+++ Checks/check_service.ps1
@@ -10,17 +10,20 @@ $srv = Get-Service -Name $Name
 if ($srv.Status -ne 'Running')
 {
     $sensu_status = 2
+    $output = "Serivce Check FAIL: The $($Name) service is not running."
 }
 else
 {
     $sensu_status = 0
+    $output = "Serivce Check OK: The $($Name) service is running."
 }
 
 $metricObject = @{
     service_display_name = $srv.DisplayName
     dependant_services = $srv.DependentServices | Select-Object -ExpandProperty Name
     required_services = $srv.RequiredServices | Select-Object -ExpandProperty Name
-    output = $srv.Status.ToString()
+    service_status = $srv.Status.ToString()
+    output = $output
     status = $sensu_status
 }
 

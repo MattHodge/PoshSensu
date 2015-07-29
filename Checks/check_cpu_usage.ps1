eaa947a68ae579e98dd3004e7175333fdbca61cd
Matt Hodge <matthodge@gmail.com>
2015-07-29 12:24:16 -0500

    Making check_cpu_usage.ps1 work with multiple physical processesors and add processer avaliability information

--- Checks/check_cpu_usage.ps1
+++ Checks/check_cpu_usage.ps1
@@ -11,30 +11,49 @@
     $ErrorThreshold = 90
 )
 
-$cpu = Get-WmiObject win32_processor
+$cpu = Get-CimInstance -ClassName CIM_Processor
+$cpuUsageTotal = 0
+$numberOfCPUs = 0
+$metricObject = @{}
 
-if ($cpu.LoadPercentage -ge $ErrorThreshold)
+ForEach ($c in $cpu)
+{
+    # Add up the load percentage for each CPU
+    $cpuUsageTotal += $c.LoadPercentage
+    
+    # Add the number of CPU's - didn't work with the $cpu.Lenght Property 
+    $numberOfCPUs++
+
+    # Add info on this CPU
+
+    $metricObject."$($c.DeviceID.ToLower())_name" = $c.Name
+    $metricObject."$($c.DeviceID.ToLower())_cores" = $c.NumberOfCores
+    $metricObject."$($c.DeviceID.ToLower())_logicalprocessors" = $c.NumberOfLogicalProcessors
+    $metricObject."$($c.DeviceID.ToLower())_loadpercentage" = $c.LoadPercentage
+    $metricObject."$($c.DeviceID.ToLower())_availability" = $c.Availability
+}
+
+# Get average across all CPU's
+$avgLoad = $cpuUsageTotal / $numberOfCPUs
+
+if ($avgLoad -ge $ErrorThreshold)
 {
     $sensu_status = 2
-    $output = "CPU Load Percentage Over Error Threshold of $($ErrorThreshold)% - Currently $($cpu.LoadPercentage)%"
+    $output = "CPU Average Load Percentage Over Error Threshold of $($ErrorThreshold)% - Currently $($avgLoad)%"
 }
-elseif ($cpu.LoadPercentage -gt $WarningThreshold)
+elseif ($avgLoad -gt $WarningThreshold)
 {
     $sensu_status = 1
-    $output = "CPU Load Percentage Over Warning Threshold of $($WarningThreshold)% - Currently $($cpu.LoadPercentage)%"
+    $output = "CPU Average Load Percentage Over Warning Threshold of $($WarningThreshold)% - Currently $($avgLoad)%"
 }
 else
 {
     $sensu_status = 0
-    $output = "CPU Load Percentage OK - $($cpu.LoadPercentage)%"
+    $output = "CPU Average Load Percentage OK - $($avgLoad)%"
 }
 
-$metricObject = @{
-    cpu_name = $cpu.Name
-    cpu_cores = $cpu.NumberOfCores
-    cpu_logicalprocessors = $cpu.NumberOfLogicalProcessors
-    output = $output
-    status = $sensu_status
-}
+$metricObject.output = $output
+$metricObject.status = $sensu_status
+$metricObject.cim_class_url = "https://msdn.microsoft.com/en-us/library/aa387978(v=vs.85).aspx"
 
 return $metricObject

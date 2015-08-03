f365dbeb5ff93cd3a49b982fbcf7bb6b3bda76f8
Matt Hodge <matthodge@gmail.com>
2015-08-04 09:08:34 +1000

    Added check Test-TCPConnection

---
+++ Checks/Test-TCPConnection.ps1
@@ -0,0 +1,41 @@
+﻿function Test-TCPConnection
+{
+    Param(
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $ComputerName,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [ValidateRange(1,65535)]
+        [int]
+        $Port
+    )
+
+    $test = New-Object System.Net.Sockets.TcpClient
+
+    $metricObject = @{}
+
+    Try
+    {
+        $test.Connect($ComputerName, $Port);
+        $sensu_status = 0
+        $output = "TCP connection successful to $($ComputerName):$($Port)"
+    }
+    Catch
+    {
+        $sensu_status = 2
+        $output = "TCP connection failure to $($ComputerName):$($Port)"
+    }
+    Finally
+    {
+        $test.Dispose();
+        $metricObject.status = $sensu_status
+        $metricObject.output = $output
+    }
+
+    return $metricObject
+}
